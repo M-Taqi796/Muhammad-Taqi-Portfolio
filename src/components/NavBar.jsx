3ce70aaf92200a823d9d535ebce25a7c146f9d50
M-Taqi796 <itstaqi2919@gmail.com>
2025-08-12 06:11:44 +0500

    Updated: Extra Padding Removed from Nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="flex justify-between px-32 py-6 mb-20">
+    <nav className="flex justify-between py-6 mb-20">
       <Link to="/">
         <h1 className="font-Nura text-2xl font-bold">
           <span className="text-transparent [-webkit-text-stroke:1px_white]">
